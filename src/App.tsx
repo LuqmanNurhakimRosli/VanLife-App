8035026cfda73947426f1fba417f1f502e5613cb
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-19 22:53:19 +0800

    okay

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Vans from './components/pages/Vans'
 import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom'
 import './App.css'
 
+
 function App() {
   return (
     <Router>
