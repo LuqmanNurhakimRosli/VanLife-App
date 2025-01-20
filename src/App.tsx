3eea06628eb5850dd56c6e0708fcb5063f7ddd86
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-20 11:50:42 +0800

    tanavh baru

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import About from './components/pages/About'
-import Home from './components/pages/Home'
+import Home from './components/pages/home' // Ensure this matches the actual file name
 import Vans from './components/pages/Vans'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
@@ -7,12 +7,12 @@ import './App.css'
 function App() {
   return (
     <Router>
-      <header className="App-header flex justify-between items-center p-6 bg-white shadow-md">
-        <Link className='logo-brand text-2xl font-bold' to="/">#VanLife</Link>
-        <nav className='nav flex space-x-4'>
-          <Link to='/' className="text-lg">Home</Link>
-          <Link to='/about' className="text-lg">About</Link>
-          <Link to='/vans' className="text-lg">Vans</Link>
+      <header className="App-header">
+        <Link className='logo-brand' to="/">#VanLife</Link>
+        <nav className='nav'>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
       <Routes>
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div className="home-container flex flex-col items-center text-center p-6">
-            <h1 className="text-4xl font-bold mb-4">You got the travel plans, we got the travel vans.</h1>
-            <p className="text-lg mb-6">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <div className="home-container">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans" className="button">Find your van</Link>
         </div>
     )
