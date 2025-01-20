d2ba0adb672042832d6db98f651fa71cff0342df
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-20 15:33:05 +0800

    upload

---
+++ src/components/pages/data/vansData.js
@@ -0,0 +1,28 @@
+const vansData = {
+    "1": {
+        id: "1",
+        name: "Modest Explorer",
+        price: 60,
+        description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
+        imageUrl: "/modest-explorer.png",
+        type: "simple"
+    },
+    "2": {
+        id: "2",
+        name: "Beach Bum",
+        price: 80,
+        description: "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
+        imageUrl: "/beach-bum.png",
+        type: "rugged"
+    },
+    "3": {
+        id: "3",
+        name: "Reliable Red",
+        price: 100,
+        description: "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, perfect for making meals you can enjoy on the road.",
+        imageUrl: "/reliable-red.png",
+        type: "luxury"
+    }
+};
+
+export default vansData
