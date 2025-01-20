47ba1a8658355718085e784d4cf7f73903287b7e
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-20 15:00:32 +0800

    iok

--- src/components/pages/VanDetail.tsx
+++ src/components/pages/VanDetail.tsx
@@ -1,6 +1,39 @@
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import './css/vandetail.css'
+
+interface Van {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    imageUrl: string;
+    type: string;
+}
 
 export default function VanDetail() {
+    const params = useParams()
+    const [van, setVan] = useState<Van | null>(null)
+
+    useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
     return (
-        <div>VanDetail</div>
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} alt={van.name} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p className="van-description">{van.description}</p>
+                </div>
+            ) : (
+                <h2>Loading...</h2>
+            )}
+        </div>
     )
 }
