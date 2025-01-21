c48c0c179318bd96695bdaa320c8a795ed7e2035
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-21 11:25:16 +0800

    salah ni

--- src/components/HostLayout.tsx
+++ src/components/HostLayout.tsx
@@ -1,8 +1,16 @@
-import {Outlet} from 'react-router-dom'
+import {Outlet, Link} from 'react-router-dom'
+import './css/hostlayout.css'
 
 function HostLayout() {
   return (
     <div>
+        <header>
+        <nav className="nav">
+            <Link to="/host">Dashboard</Link>
+            <Link to="/host/income">Income</Link>
+            <Link to="/host/reviews">Reviews</Link>
+        </nav>
+    </header>
         <Outlet />
     </div>
   )
