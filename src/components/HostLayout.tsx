16796a36ee4bb4bb1345863aacc5d6249d9e5081
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-21 11:31:16 +0800

    update

--- src/components/HostLayout.tsx
+++ src/components/HostLayout.tsx
@@ -4,13 +4,11 @@ import './css/hostlayout.css'
 function HostLayout() {
   return (
     <div>
-        <header>
-        <nav className="nav">
+        <nav className="host-nav">
             <Link to="/host">Dashboard</Link>
             <Link to="/host/income">Income</Link>
             <Link to="/host/reviews">Reviews</Link>
         </nav>
-    </header>
         <Outlet />
     </div>
   )
