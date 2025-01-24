ee66e323a4550c57c30f11cc6893bf226d425cbc
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-24 16:01:17 +0800

    error page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,14 @@
 import {Link} from 'react-router-dom'
+import './NotFound.css';
 
 function NotFound() {
-  return (
-    <div>
-        <div>
-            <h1>404 Not Found</h1>
-            <Link to="/">
-                Back to Home
-            </Link>
+    return (
+        <div className="not-found-container">
+            <h1>404 - Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+            <Link to="/">Go back to home</Link>
         </div>
-    </div>
-  )
+    );
 }
 
 export default NotFound
