ede8fa9c8dbff8dc00b1862209d62bd1d6925208
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-27 12:11:34 +0800

    tambah alogin auth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,47 +1,78 @@
-import { useState } from "react"
-import { useLocation } from "react-router-dom"
-import { loginUser } from "../api"
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { loginUser } from "../api";
 import './css/Login.css';
 
 interface LoginResponse {
-    user: { id: string; name: string; }; 
+    user: { id: string; name: string; };
     token: string;
 }
 
-function Login() {
-    const [loginFormData, setLoginFormData] = useState({email: "", password: ""})
+interface ErrorResponse {
+    message: string;
+}
 
-    const location = useLocation()
-    console.log(location)
+function Login() {
+    const [loginFormData, setLoginFormData] = useState({ email: "", password: "" });
+    const [status, setStatus] = useState<"idle" | "submitting">("idle");
+    const [error, setError] = useState<ErrorResponse | null>(null);
+
+    const location = useLocation();
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+        e.preventDefault();
+        setStatus("submitting");
+        setError(null);
+
         loginUser(loginFormData)
-            .then((data: LoginResponse) => console.log(data))
+            .then((data: LoginResponse) => {
+                console.log(data);
+                setError(null);
+            })
+            .catch((error: ErrorResponse) => {
+                setError(error);
+            })
+            .finally(() => setStatus("idle"));
     }
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const {name, value} = e.target
-        setLoginFormData (prev => ({
-            ...prev, [name] : value
-        }))
+        const { name, value } = e.target;
+        setLoginFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
     }
 
-  return (
-    <div className="login-container">
-      { location.state?.message && <h3>{location.state.message}</h3> }
-      <h1>Sign in to your account</h1>
-      <form onSubmit={handleSubmit} className="login-form">
-        
-        <input name="email" onChange={handleChange} type="email" placeholder="Email" value={loginFormData.email}  />
-        <input name="password" onChange = {handleChange} type="password" placeholder="Password" value={loginFormData.password}  />
+    return (
+        <div className="login-container">
+            {location.state?.message && <h3>{location.state.message}</h3>}
+            <h1>Sign in to your account</h1>
+            {error?.message && <h3>{error.message}</h3>}
 
-        <button type="submit">Login</button>
+            <form onSubmit={handleSubmit} className="login-form">
+                <input
+                    name="email"
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Email"
+                    value={loginFormData.email}
+                    disabled={status === "submitting"}
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    type="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                    disabled={status === "submitting"}
+                />
 
-      </form>
-
-    </div>
-  )
+                <button disabled={status === "submitting"}>
+                    {status === "submitting" ? "Logging in..." : "Log in"}
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
