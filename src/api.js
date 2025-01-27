ea9269f13cb9913c10d71aa004ada0b1b8d34253
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-27 10:21:53 +0800

    basus

--- src/api.js
+++ src/api.js
@@ -1,15 +1,7 @@
 export async function getVans() {
 
     const res = await fetch("api/vans")
-    if (!res.ok) {
-        throw {
-            message: "Failed to fretch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-
-
+    
     
     const data = await res.json();
 
