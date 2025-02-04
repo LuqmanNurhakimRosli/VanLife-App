fd53ba964c5b17e4c577a0798f7d5115d756e5da
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-02-04 09:28:25 +0800

    udates

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './firebase/firebase'
+import {BrowserRouter} from 'react-router-dom'
+import {AuthProvider} from './contexts/AuthContext'
 import './index.css'
 import App from './App.tsx'
 import './server'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
