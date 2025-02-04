dfde48466ba4157798f7a96d8cbe336771272ec0
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-02-04 09:31:18 +0800

    authcontext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,30 @@
+// AuthContext.tsx
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase"
+import { onAuthStateChanged, User } from "firebase/auth";
+
+interface AuthContextType {
+  user: User | null;
+}
+
+const AuthContext = createContext<AuthContextType>({ user: null });
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    // Listen for auth state changes
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
